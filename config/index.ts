ed801635f238b143b76e5140016a5f91278b8cf8
Vipshal WG <119661093+vipshal-123@users.noreply.github.com>
2025-09-24 19:05:26 +0530

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -2,5 +2,5 @@ export default {
     PORT: process.env.PORT as string,
     MONGO_URI: process.env.MONGO_URI as string,
     API_HOST: process.env.API_HOST as string,
-    CORS_ORIGIN: ['http://localhost:3000', 'https://admin.socket.io/#/', 'https://bingo-frontend-la1y.onrender.com],
+    CORS_ORIGIN: ['http://localhost:3000', 'https://admin.socket.io/#/', 'https://bingo-frontend-la1y.onrender.com'],
 }
